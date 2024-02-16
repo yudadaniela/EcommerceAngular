20acd440dae420e80fe1ce416e080946780c1fcf
Daniela <yudadaniela@hotmail.com>
2024-02-16 09:42:39 -0500

    configuracion del nombre del usuario y boton logout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from '../../../Models/user';
 
 @Component({
   selector: 'app-header',
@@ -8,20 +10,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  user:User|null=null
   constructor(
     private cartService: CartService,
-    private router:Router
+    private router:Router,
+    private authService:AuthService
     ) {}
   
   shoppingCounter(): number {
     return this.cartService.counter();
   }
-
+  
   isHomePage():boolean{
    return this.router.url==='/home'
   }
-  handdle(searchItem:string){
-  console.log('termino de la busqueda', searchItem);
-  
+  // handdle(searchItem:string){
+  // console.log('termino de la busqueda', searchItem);
+  // }
+
+  currentUser(){
+    this.user = this.authService.getUser()
+    return this.user
+  }
+  signOff(){
+    console.log(this.authService.logout());
+    
+    this.router.navigate([''])
+    return this.authService.logout()
   }
 }
